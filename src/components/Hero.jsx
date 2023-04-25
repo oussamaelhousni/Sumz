dc71b8278fc0bf98ac652bcd214d8712556bfe2b
oussamaelhousni <oussamaelhousnioe@gmail.com>
2023-04-25 01:16:13 +0100

    main commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import {logo} from '../assets'
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} className='w-28 object-contain' alt="sumz_logo" />
+            <button type='button' className="black_btn" onClick={()=>window
+            .open("https://github.com/oussamaelhousni/Sumz")}>GitHub</button>
+        </nav>
+        <h1 className="head_text">Summarize Articles with <br className='max-md:hidden'/>
+            <span className='orange_gradient'>OpenAi GPT-4</span>
+        </h1>
+        <h2 className="desc">
+            Simplify your reading with Summize, an open-source article summarizer that transforms lengthly articles into clear and concise summaries
+        </h2>
+    </header>
+  )
+}
+
+export default Hero
